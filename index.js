4ca355a6979e45845fb8e0d2c2a25197d570bf82
samba325 <asiva325@gmail.com>
2017-11-30 23:03:38 +0530

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
  
 app.use(express.static(__dirname + '/client/dist'));
-// app.get('*',(req,res) => {
-// 	res.sendFile(path.join(__dirname + '/client/dist/index.html'));
-// })
+app.get('*',(req,res) => {
+	res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+})
  
  
 
